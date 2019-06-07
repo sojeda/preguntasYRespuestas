cfad3ec5b7a58caa0f20b8c12a41b400bb78f2cb
ketzal88 <gabrielucc@gmail.com>
2019-06-06 21:45:15 -0300

    preguntas gabriel

--- script.js
+++ script.js
@@ -131,5 +131,65 @@ var preguntas = [
             d: 'Mario Bross',
         },
         correcta: 'b',
+    },
+    {
+        question = "¿En qué día se llevó a cabo la Revolución de mayo?",
+        answers: {
+        a = "10 de mayo",
+        b = "9 de julio",
+        c = "20 de junio",
+        d = '25 de mayo',
+        },
+        wright: 'd',
+    },
+    {
+        question = "¿En qué día se firmó el acta de la Independencia?",
+        answers: {
+        a = "10 de mayo",
+        b = "9 de julio",
+        c = "20 de junio",
+        d = '25 de mayo',
+        },
+        wright: 'd',
+    },
+    {
+        question = "¿En qué año se prohibieron las corridas de toros en Buenos Aires",
+        answers: {
+        a = "1810",
+        b = "1565",
+        c = "1954",
+        d = '1822',
+        },
+        wright: 'd',
+    },
+    {
+        question = "¿En qué año Sarmiento fue nombrado presidente?",
+        answers: {
+        a = "1810",
+        b = "1880",
+        c = "1860",
+        d = '1915',
+        },
+        wright: 'b',
+    },
+    {
+        question = "¿Cuándo se prohibió la esclavitud en la Argentina?",
+        answers: {
+        a = "1813",
+        b = "1822",
+        c = "1545",
+        d = '1860',
+        },
+        wright: 'a',
+    },
+    {
+        question = "¿Quién fundó el primer asentamiento en Buenos Aires?",
+        answers: {
+        a = "Pedro de Mendoza",
+        b = "Juan de Garay",
+        c = "Manuel Belgrano",
+        d = 'Perón',
+        },
+        wright: 'a',
     }
 ];
